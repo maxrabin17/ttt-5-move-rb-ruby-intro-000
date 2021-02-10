a1ba77c93066ef5aeda0328434a8794f2a93458a
Max Rabin <max.rabin17@gmail.com>
2021-02-10 17:59:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,6 +7,6 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-input_to_index 5 .to_i
-display_board
-end-of-input
+def input_to_index(user_input)
+  user_input.to_i
+end
